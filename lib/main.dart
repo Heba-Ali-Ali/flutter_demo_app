8f118e32752b439674e120081d98017fa8f7887a
Heba Ali <lovelyhebaali@gmail.com>
2020-06-13 00:45:16 +0200

    this is first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Demo"),
+      ),
+      body: Container(
+        child: Text("Hello World"),
+        width: 50,
+        height: 50,
+        color: Colors.red,
+      ),
+    );
+  }
+}
